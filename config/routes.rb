db5f566fd38c68844877dd81b9cecf384792f0f6
Masurun <wataru.5756@gmail.com>
2021-03-13 13:15:36 +0900

    homeの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'records#index'
+  root to: 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :records
   resources :foods
+  get 'homes/index'
 end
